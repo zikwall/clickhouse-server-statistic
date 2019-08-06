fe8342f975530779590d8d156b48a114a075b0cb
Andreev Andrey <andrey.kapitonov.96@gmail.com>
2019-08-06 14:22:18 +0300

    Add local (private) configuration files

---
+++ config/web-local.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=databasename',
+            'username' => '***',
+            'password' => '***',
+            'charset' => 'utf8',
+        ]
+    ]
+];
