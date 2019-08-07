49c38558b8d9315a1d0d60c900dcb53a18b28be4
Andreev Andrey <andrey.kapitonov.96@gmail.com>
2019-08-07 15:59:31 +0300

    Add auth permissions

--- modules/rest/Bootstrap.php
+++ modules/rest/Bootstrap.php
@@ -12,6 +12,7 @@ class Bootstrap implements BootstrapInterface
             // Auth
             ['pattern' => 'api/v1/auth/login/', 'route' => 'rest/auth/auth/index', 'verb' => ['POST', 'OPTIONS']],
             ['pattern' => 'api/v1/data/get/', 'route' => 'rest/data/data/index', 'verb' => ['GET']],
+            ['pattern' => 'api/v1/auth/permissions/', 'route' => 'rest/auth/auth/permissions', 'verb' => ['GET']],
         ], true);
     }
 }
