c99971ed03d53acf2a35b564ed5e3cc48e6cf723
Andreev Andrey <andrey.kapitonov.96@gmail.com>
2019-08-09 16:50:46 +0300

    Add pre-options handle

--- modules/rest/controllers/asn/AsnController.php
+++ modules/rest/controllers/asn/AsnController.php
@@ -4,15 +4,29 @@ namespace app\modules\rest\controllers\asn;
 
 use app\modules\rest\components\BaseController;
 use app\modules\rest\models\Monit;
+use Yii;
 
 class AsnController extends BaseController
 {
+    public function beforeAction($action): bool
+    {
+        if (Yii::$app->request->getIsOptions()) {
+            return true;
+        }
+
+        return parent::beforeAction($action);
+    }
+
     /**
-     * @return \yii\web\Response
+     * @return bool|\yii\web\Response
      * @throws \MaxMind\Db\Reader\InvalidDatabaseException
      */
     public function actionExample()
     {
+        if (Yii::$app->request->getIsOptions()) {
+            return true;
+        }
+
         ini_set('memory_limit', '2044M');
         ini_set('max_execution_time', '1000');
 
