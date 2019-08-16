e41d1562e4ba1d9810f393c777ed7df916aa482f
Andreev Andrey <andrey.kapitonov.96@gmail.com>
2019-08-16 16:56:12 +0300

    Увеличено время жизни токена авторизации

--- modules/rest/Module.php
+++ modules/rest/Module.php
@@ -10,6 +10,6 @@ class Module extends \app\modules\core\components\base\Module
     public $enabledForAllUsers = false;
     public $disabledUsers = [];
     public $jwtKey = 'shwjskwskwsluhrnfrlkfeHWJSKuwmswkUWKwnskiwswswlkmdc';
-    public $jwtExpire = 120;
+    public $jwtExpire = 420;
     public $enableBasicAuth = false;
 }
