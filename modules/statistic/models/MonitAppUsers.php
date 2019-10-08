5732e04f924395087b0885a8506e919b1fc66876
Andreev Andrey <andrey.kapitonov.96@gmail.com>
2019-10-08 11:32:52 +0300

    :pencil2: Fix five timezone

--- modules/statistic/models/MonitAppUsers.php
+++ modules/statistic/models/MonitAppUsers.php
@@ -62,7 +62,11 @@ class MonitAppUsers extends CHBaseModel
 
     public static function getAllTimezones()
     {
-        return ['0', '2', '3', '4', '5', '6', '7', '8'];
+        return [
+            '-11', '-10', '-9', '-8', '-7', '-6', '-5', '-4', '-3', '-2', '-1',
+            '0',
+            '1', '2', '3', '4', '5', '5.5', '6', '7', '8', '9', '10', '11', '12'
+        ];
     }
 
     public static function getTimeZoneUsers($app, $dayBegin, $dayEnd, $eventType)
